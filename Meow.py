ee60831bf1aab72562d0b3e022d7e2a4c339d569
ellilin <76488484+ellilin@users.noreply.github.com>
2023-03-23 08:21:03 +0300

    Meow.py

---
+++ Meow.py
@@ -0,0 +1,15 @@
+p=int(input()) 
+sf=int(input()) 
+a=int('1'*185) 
+ma=[0]*185 
+print(ma) 
+sf-=1 
+sf=bin(sf)[2:])
+p=int(bin(p)[2:]) 
+sf=int((185-len(sf))*'0'+sf) 
+mask=bin(int(str(a),2)-int(str(sf),2))[2:])
+print(p) 
+print(sf) 
+print(mask) 
+for i in range(1,186): 
+ if()
