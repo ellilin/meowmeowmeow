b7e4db1a9a7156f3b96f9c8de907b36c75b25cbc
ellilin <76488484+ellilin@users.noreply.github.com>
2023-03-23 09:49:07 +0300

    Update Meow.py

--- Meow.py
+++ Meow.py
@@ -1,19 +1,17 @@
-p=int(input()) 
-sf=int(input()) 
-a=int('1'*185) 
-ma=[0]*185 
-print(ma) 
-sf-=1 
-sf=bin(sf)[2:])
-p=int(bin(p)[2:]) 
-sf=int((185-len(sf))*'0'+sf) 
-mask=bin(int(str(a),2)-int(str(sf),2))[2:])
-print(p) 
-print(sf) 
-print(mask) 
-for i in range(1,186): 
- if(ma[185-i] != mask[185-i]){
-   s+='1'
-   
-   
-  
+p = int(input())
+sf = int(input())
+a = int("1" * 185)
+ma = [0] * 185
+print(ma)
+sf -= 1
+sf = bin(sf)[2:]
+p = int(bin(p)[2:])
+sf = int((185 - len(sf)) * "0" + sf)
+mask = bin(int(str(a), 2) - int(str(sf), 2))[2:]
+print(p)
+print(sf)
+print(mask)
+for i in range(1, 186):
+    if ma[185 - i] != mask[185 - i]:
+        s += "1"
+        ma[-i:(-i + len(mask))] = map(int, list(str(bin(p))))
